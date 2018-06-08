f046b54391c58d5713c290c6523cea4cadbb8484
ngyl88 <ng_yl88@hotmail.com>
2018-06-08 18:17:34 +0800

    todo: remove done items on clicking x button

--- script.js
+++ script.js
@@ -5,12 +5,29 @@ const tasks = [
 	'give feedback',
 ];
 
+//additional
+const addDeleteBtn = (parent) => {
+	const deleteBtn = document.createElement('button');
+	deleteBtn.innerText = 'X';
+	deleteBtn.classList.add('delete');
+
+	deleteBtn.addEventListener('click', (evt) => {
+		console.log(evt.target);
+	});
+
+	parent.appendChild(deleteBtn);
+}
+
 // 1. for each task
 // 1a. create li
 // 1b. append it to #todo-list
 tasks.forEach((task) => {
 	const newTask = document.createElement('li');
 	newTask.innerText = task;
+	
+	// additional
+	addDeleteBtn(newTask);
+	
 	document.querySelector('#todo-list').appendChild(newTask);
 });
 
@@ -43,40 +60,53 @@ document.querySelectorAll('li').forEach((task) => {
 
 // ADD MORE TO-DO ITEMS
 // 5. Add an input HTML and a button element
-/*const input = document.createElement('input');
+const input = document.createElement('input');
+input.id = 'new-todo';
 document.querySelector('body').appendChild(input);
 
 const button = document.createElement('button');
+button.id = 'btn-add';
 button.innerText = 'Add Todo';
-document.querySelector('body').appendChild(button);*/
+document.querySelector('body').appendChild(button);
 
 // 6a. Create function to get value from <input> element and add to ul#todo-list
-const addTodo = () => {
+const addTodo = (evt) => {
+	console.log(evt);
+
 	const inputBox = document.querySelector('input#new-todo');
 	const newTodoItem = inputBox.value;
-	inputBox.value = '';
-	console.log('Last entered todo:', newTodoItem);
-	// i.  createElement
-	// ii. changeAttributes
-	// iii.appendChild to ul
-	const newTodoItemLi = document.createElement('li');
-	newTodoItemLi.textContent = newTodoItem;
-	// ** Need to add event listener
-	newTodoItemLi.addEventListener('click', onTaskClicked);
-	document.querySelector('ul').appendChild(newTodoItemLi);
+
+	if(newTodoItem === '') {
+		alert('Empty task!');
+	} else {
+		inputBox.value = '';
+		console.log('Last entered todo:', newTodoItem);
+		// i.  createElement
+		// ii. changeAttributes
+		// iii.appendChild to ul
+		const newTodoItemLi = document.createElement('li');
+		newTodoItemLi.textContent = newTodoItem;
+		// ** Need to add event listener
+		newTodoItemLi.addEventListener('click', onTaskClicked);
+		document.querySelector('ul').appendChild(newTodoItemLi);
+		// additional
+		addDeleteBtn(newTodoItemLi);
+	}
+	
 };
 
 // 6b. Add event listener, on click
-document.querySelector('#btn-add').addEventListener('click', () => console.log('button clicked!'));	// checking
-document.querySelector('#btn-add').addEventListener('click', addTodo);
-
-//7. Add css styling and make this a nice todo list that you can show off!
+// Checking
+//document.querySelector('#btn-add').addEventListener('click', () => console.log('button clicked!'));
+document.querySelector('#btn-add').addEventListener('click', (evt) => addTodo(evt));
 
 //8. Bonus: Allow user to create a todo when they press the Enter key
 document.querySelector('#new-todo').addEventListener('keypress', (evt) => {
-	console.log('Keypress detected with charCode:', evt.charCode);	// checking
+	// Checking
+	//console.log('Keypress detected with charCode:', evt.charCode);
+
+	// Entered detected
 	if(evt.charCode === 13) {
-		console.log('Entered detected!');
-		addTodo();
+		addTodo(evt);
 	}
 });
